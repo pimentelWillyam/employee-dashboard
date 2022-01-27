d841a38f73d6afa15eade413af6944ce9fda1985
Willyam <pimentelwillyam@gmail.com>
2022-01-26 23:23:20 -0300

    dasdas

--- client/src/pages/Dashboard/index.jsx
+++ client/src/pages/Dashboard/index.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 import {Link} from 'react-router-dom';
 
-import {Container} from 'react-bootstrap/';
-import
+import Container from 'react-bootstrap/Container';
+import StudentList from '../../components/StudentList';
 
 function Dashboard() {
         return (
             <Container>
               <Header></Header>
+              <StudentList></StudentList>
+              <Footer></Footer>
             </Container>
-
-            <
         );
       }
+
 export default Dashboard;
